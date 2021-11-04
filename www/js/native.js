1c6c15b775c76264eb9e25ca75c5ebf23b250891
GB-Titi <tristanmatteoli@gmail.com>
2021-11-04 11:30:06 +0100

    modif page offline

--- www/js/native.js
+++ www/js/native.js
@@ -1,5 +1,5 @@
 const deviceReady = () => {
     window.addEventListener("batterystatus", onBatteryStatus, false);
-    window.addEventListener("offline", checkConnection, false);
+    window.addEventListener("sendOfflinePage ", checkConnection, false);
     window.addEventListener("online", checkConnection, true);
 };
